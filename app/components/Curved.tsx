519f22bfa2f286c16f89562d99eefe3b898aa2c4
MohsenDorado <161645984+MohsenDorado@users.noreply.github.com>
2024-08-15 18:32:15 +0330

    a try for mobile design

--- app/components/Curved.tsx
+++ app/components/Curved.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Curved = () => {
   return (
-    <div className="absolute top-0 left-0 w-full h-[1000px] bg-[#f3f4f6] mt-[450px]  z-10 scaled"></div>
+    <div className="absolute top-0 left-0 w-full h-[1000px] bg-[#f3f4f6] mt-[450px] scaledsm   z-10 scaled"></div>
   );
 };
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout({
     <body className="">
       <Header/>
       {children}
-      <MobileFooter/>
 
     </body>
   </html>
